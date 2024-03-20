88876968c858d439f80dfc0a6b53056616ad053c
sergeemmanuelk <serge_em@outlook.fr>
2024-03-20 22:01:29 +0000

    initial commit

---
+++ src/main/java/dev/skonan/spotifyclonebackend/SpotifyCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.skonan.spotifyclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/skonan/spotifyclonebackend/SpotifyCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skonan.spotifyclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
